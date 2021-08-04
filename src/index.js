a6eb0bf979cab2edfde8e390d3172c04caebe8f0
PolinaStamenova <polina.stamenova@gmail.com>
2021-08-04 17:18:18 +0300

    Solve conflict

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
-import _ from "lodash";
-import "./style.css";
-=======
 // import _ from "lodash";
-import './style.css';
->>>>>>> d0c701e941e9aad6a2e1f0f5706c746d455c25b9
+import "./style.css";
