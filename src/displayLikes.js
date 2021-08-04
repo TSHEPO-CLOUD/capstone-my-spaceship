bdec1cbedfc7895353342e15a81be03e8d862e58
PolinaStamenova <polina.stamenova@gmail.com>
2021-08-04 19:05:45 +0300

    Fix linter

--- src/displayLikes.js
+++ src/displayLikes.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
+const likeUrl = 'FPwS7kLi1stB3QIQqLTK';
 const getLikes = async (name) => {
   let res = await axios.get(likeUrl);
-  res = res.data.filter((data) => data.item_id == name);
+  res = res.data.filter((data) => data.item_id === name);
   const result = res.length > 0 ? res[0].likes : 0;
   return result;
 };
