8db3b8f612fbfc493744e83367ad48b82347f6a9
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-08-05 15:55:12 +0200

    Fix linters

--- src/addComment.js
+++ src/addComment.js
@@ -1,26 +1,26 @@
-import axios from "axios";
-import commentLoader from "./commentLoader.js";
+import axios from 'axios';
+import commentLoader from './commentLoader.js';
 
 const addComment = (pokeName, commentUrl) => {
-  const container = document.createElement("div");
-  container.classList.add("popup-container");
-  const header = document.createElement("h4");
-  header.innerHTML = "Add a comment";
+  const container = document.createElement('div');
+  container.classList.add('popup-container');
+  const header = document.createElement('h4');
+  header.innerHTML = 'Add a comment';
   container.appendChild(header);
-  const popupComment = document.createElement("div");
-  popupComment.classList.add("popup-comment");
-  const nameInput = document.createElement("input");
-  nameInput.classList.add("name-input");
-  const commentInput = document.createElement("input");
-  commentInput.classList.add("comment-input");
-  nameInput.type = "text";
-  commentInput.type = "text";
-  nameInput.placeholder = "Your name";
-  commentInput.placeholder = "Your comment";
-  const btn = document.createElement("button");
-  btn.innerHTML = "Comment";
-  btn.classList.add("popup-cemment-btn");
-  btn.addEventListener("click", async () => {
+  const popupComment = document.createElement('div');
+  popupComment.classList.add('popup-comment');
+  const nameInput = document.createElement('input');
+  nameInput.classList.add('name-input');
+  const commentInput = document.createElement('input');
+  commentInput.classList.add('comment-input');
+  nameInput.type = 'text';
+  commentInput.type = 'text';
+  nameInput.placeholder = 'Your name';
+  commentInput.placeholder = 'Your comment';
+  const btn = document.createElement('button');
+  btn.innerHTML = 'Comment';
+  btn.classList.add('popup-cemment-btn');
+  btn.addEventListener('click', async () => {
     await axios
       .post(commentUrl, {
         comment: commentInput.value,
--- src/displayPopup.js
+++ src/displayPopup.js
@@ -1,14 +1,14 @@
-import axios from "axios";
-import commentSection from "./commentSection.js";
-import addComment from "./addComment.js";
+import axios from 'axios';
+import commentSection from './commentSection.js';
+import addComment from './addComment.js';
 
-export const apiKey = "FPwS7kLi1stB3QIQqLTK";
+export const apiKey = 'FPwS7kLi1stB3QIQqLTK';
 
 const commentUrl = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${apiKey}/comments`;
 
 const displayPopup = async (pokeurl) => {
-  const bgDiv = document.createElement("div");
-  bgDiv.classList.add("bg-div");
+  const bgDiv = document.createElement('div');
+  bgDiv.classList.add('bg-div');
   const res = await axios.get(pokeurl);
   const image = res.data.sprites.front_default;
   const { name } = res.data;
@@ -16,31 +16,31 @@ const displayPopup = async (pokeurl) => {
   const { height } = res.data;
   const type = res.data.types[0].type.name;
 
-  const body = document.getElementById("body");
-  const popupDiv = document.createElement("div");
-  popupDiv.classList.add("popup-div");
+  const body = document.getElementById('body');
+  const popupDiv = document.createElement('div');
+  popupDiv.classList.add('popup-div');
 
-  const div = document.createElement("div");
-  const closeIcon = document.createElement("i");
-  closeIcon.classList.add("fas", "fa-times");
-  const pokeImg = document.createElement("img");
-  pokeImg.classList.add("popup-image");
+  const div = document.createElement('div');
+  const closeIcon = document.createElement('i');
+  closeIcon.classList.add('fas', 'fa-times');
+  const pokeImg = document.createElement('img');
+  pokeImg.classList.add('popup-image');
   pokeImg.src = image;
-  closeIcon.addEventListener("click", () => {
+  closeIcon.addEventListener('click', () => {
     body.removeChild(bgDiv);
   });
 
-  const pokeName = document.createElement("p");
-  pokeName.classList.add("popup-name");
+  const pokeName = document.createElement('p');
+  pokeName.classList.add('popup-name');
   pokeName.textContent = name;
-  const measureH = document.createElement("p");
-  measureH.classList.add("popup-height");
-  const measureW = document.createElement("p");
-  const pokeType = document.createElement("p");
+  const measureH = document.createElement('p');
+  measureH.classList.add('popup-height');
+  const measureW = document.createElement('p');
+  const pokeType = document.createElement('p');
   measureH.innerHTML = `Height: ${height}`;
   measureW.innerHTML = `Weight: ${weight}`;
   pokeType.innerHTML = `Type: ${type}`;
-  pokeType.classList.add("popup-type");
+  pokeType.classList.add('popup-type');
   div.appendChild(pokeImg);
   div.appendChild(closeIcon);
   popupDiv.appendChild(div);
