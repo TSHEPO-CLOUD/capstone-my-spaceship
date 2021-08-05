269648f7b0bfa6119c2d14860ed991a754629739
PolinaStamenova <polina.stamenova@gmail.com>
2021-08-05 14:53:45 +0300

    Add styling to the home page

--- src/getLikes.js
+++ src/getLikes.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const getLikes = async (name, likeUrl) => {
   let res = await axios.get(likeUrl);
   res = res.data.filter((data) => data.item_id === name); // returns an array with one element
-  const result = res.length > 0 ? res[0].likes : 0; // ternary operator
+  const result = res.length > 0 ? `${res[0].likes} Like(s)` : 0; // ternary operator
   return result;
 };
 export default getLikes;
